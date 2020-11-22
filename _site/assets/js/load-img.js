5420801f3fba00db784c28d0660565c76b7782f3
kaptn3 <kapitonenko.vick@ya.ru>
2020-11-22 11:30:00 +0300

    feat: new layout

--- _site/assets/js/load-img.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function () {
-    $('img').each(function () {
-        if (typeof $(this).attr("data-src") != "undefined") {
-            $(this).attr("src", $(this).attr("data-src"));
-        }
-    });
-});
--- _site/assets/js/search-form.js
+++
@@ -1,6 +0,0 @@
-jQuery("#order-top").change(function () {
-    if (jQuery("#order-top option:selected").val() == "top") {
-        jQuery("#entry-search").val("order-top");
-    }
-    jQuery("#search-form").submit();
-});
